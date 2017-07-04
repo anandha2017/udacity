6caf4c420eae40e24e31e9151ab6a306eb3eff80
Anandha Ponnampalam <github2017@ponnampalam.ca>
2017-07-04 19:09:58 +0100

    Project 2, wip. Just finished One-Hot Encoding

--- nd101 Deep Learning Nanodegree Foundation/DockerImages/projects/02-image-classification/notebooks/problem_unittests.py
+++ nd101 Deep Learning Nanodegree Foundation/DockerImages/projects/02-image-classification/notebooks/problem_unittests.py
@@ -31,7 +31,9 @@ def test_folder_path(cifar10_dataset_folder_path):
 
 def test_normalize(normalize):
     test_shape = (np.random.choice(range(1000)), 32, 32, 3)
+    #test_shape = (2, 4, 4, 3)
     test_numbers = np.random.choice(range(256), test_shape)
+    #test_numbers = np.random.choice(range(10), test_shape)
     normalize_out = normalize(test_numbers)
 
     assert type(normalize_out).__module__ == np.__name__,\
