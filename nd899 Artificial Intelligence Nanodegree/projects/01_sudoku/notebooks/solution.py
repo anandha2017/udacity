c35484ed31703284b6f890eea10689a890da2366
Anandha Ponnampalam <github2017@ponnampalam.ca>
2017-12-24 00:38:16 +0000

    Done?

--- nd899 Artificial Intelligence Nanodegree/projects/01_sudoku/notebooks/solution.py
+++ nd899 Artificial Intelligence Nanodegree/projects/01_sudoku/notebooks/solution.py
@@ -1,18 +1,28 @@
 
-from utils import *
+# coding: utf-8
 
+# In[15]:
+
+from utils import *
 
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+diagonal_units = [[r+c for r,c in zip(rows,cols)], [r+c for r,c in zip(rows,cols[::-1])]]
 
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+
+# In[16]:
+
+unitlist = row_units + column_units + square_units + diagonal_units
+
+
+# In[20]:
 
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
+
+# In[25]:
 
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
@@ -41,9 +51,23 @@ def naked_twins(values):
     and because it is simpler (since the reduce_puzzle function already calls this
     strategy repeatedly).
     """
-    # TODO: Implement this function!
-    raise NotImplementedError
 
+    # Iterate through every unit and every box in each unit
+    for unit in unitlist:
+        for box in unit:
+            # Check if there's a naked twin in the unit, naked twins => two identical values with length 2
+            if len(values[box]) == 2 and [values[bx] for bx in unit].count(values[box]) == 2:
+                # Iterate through all the boxes in the unit having a naked twin
+                for bx in unit:
+                    # If either of the naked twin's possible values exists in any other box in the unit, remove them
+                    if values[bx] != values[box] and len(set(values[box]).intersection(values[bx])) > 0:
+                        for val in values[box]:
+                            values[bx] = values[bx].replace(val, "")
+                            
+    return values
+
+
+# In[26]:
 
 def eliminate(values):
     """Apply the eliminate strategy to a Sudoku puzzle
@@ -61,9 +85,16 @@ def eliminate(values):
     dict
         The values dictionary with the assigned values eliminated from peers
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for v in values:
+        if len(values[v]) != 1:
+            continue
+        for peer in peers[v]:
+            assign_value(values, peer, values[peer].replace(values[v], ""))
+    
+    return values
 
+
+# In[27]:
 
 def only_choice(values):
     """Apply the only choice strategy to a Sudoku puzzle
@@ -85,9 +116,15 @@ def only_choice(values):
     -----
     You should be able to complete this function by copying your code from the classroom
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for unit in unitlist:
+        for digit in '123456789':
+            lst = [box for box in unit if digit in values[box]]
+            if len(lst) == 1:
+                assign_value(values, lst[0], digit)
+    return values
 
+
+# In[ ]:
 
 def reduce_puzzle(values):
     """Reduce a Sudoku puzzle by repeatedly applying all constraint strategies
@@ -103,9 +140,29 @@ def reduce_puzzle(values):
         The values dictionary after continued application of the constraint strategies
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
 
+        # Eliminate single values from peers
+        values = eliminate(values)
+        
+        # Apply the only choice contraint
+        values = only_choice(values)
+
+        # Remove naked twins
+        values = naked_twins(values)
+        
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+        
+    return values
+
+
+# In[ ]:
 
 def search(values):
     """Apply depth first search to solve Sudoku puzzles in order to solve puzzles
@@ -126,9 +183,28 @@ def search(values):
     You should be able to complete this function by copying your code from the classroom
     and extending it to call the naked twins strategy.
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    values = reduce_puzzle(values)
+    
+    if values is False:
+        return False # Failed earlier
+    
+    # If all boxes are filled with only one number, the sudoku is solved
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values
+    
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
+
+# In[ ]:
 
 def solve(grid):
     """Find the solution to a Sudoku puzzle using search and constraint propagation
@@ -150,6 +226,8 @@ def solve(grid):
     return values
 
 
+# In[ ]:
+
 if __name__ == "__main__":
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(grid2values(diag_sudoku_grid))
@@ -164,3 +242,4 @@ if __name__ == "__main__":
         pass
     except:
         print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
+
